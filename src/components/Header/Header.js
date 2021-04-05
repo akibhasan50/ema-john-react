5662012eb7b9e7d19e98f32bc8d3eac353afe384
akibhasan50 <tonuakibhasan@gmail.com>
2021-04-05 23:20:35 +0600

    design updated

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import logo from '../../images/logo.png';
+import './Header.css';
+
+export default function Header() {
+    return (
+        <div className="header">
+            <img src={logo} alt="logo"></img>
+            <nav>
+                <a href="/shop">Shop</a>
+                <a href="/review">Review</a>
+                <a href="/manage">Manage Inventory</a>
+            </nav>
+
+        </div>
+    )
+}
---
+++ src/components/Product/Product.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Product.css";
+import ReactDOM from 'react-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart  } from '@fortawesome/free-solid-svg-icons'
+
+export default function Product({ product }) {
+  console.log(product);
+  return (
+    <div className="product">
+      <div>
+        <img src={product.img} alt="" />
+      </div>
+      <div>
+        <h4 className="product-name">{product.name}</h4>
+        <br />
+        <p>
+          <small>by: {product.seller}</small>
+        </p>
+        <p>${product.price}</p>
+        <p>
+          <small>Only {product.stock} left in stock - Order soon</small>
+        </p>
+       <button className='main-button'><FontAwesomeIcon icon={faShoppingCart } />Add to cart</button>
+      </div>
+    </div>
+  );
+}
